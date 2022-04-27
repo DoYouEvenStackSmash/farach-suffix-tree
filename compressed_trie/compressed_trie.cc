4936e7dfc6b5b19508a05151ee37fda8e25f45ad
DoYouEvenStackSmash <manumoomaw@gmail.com>
2022-04-27 12:16:45 -0700

    extensions to compressed trie, collection of nodes from constant size edge table array to minimum size vector

--- compressed_trie/compressed_trie.cc
+++ compressed_trie/compressed_trie.cc
@@ -7,6 +7,7 @@ struct Node {
   int Lv;
   Edge** edge_table = nullptr;
   vector <Edge*> children;
+  int suffix_index;
   int _color;
   int _next_edge;
 };
@@ -26,7 +27,7 @@ Edge* createTrieEdge(Node* target, int* edge_label) {
   return E;
 }
 
-Node* createTrieNode(int level, bool IS_LEAF_NODE = false) {
+Node* createTrieNode(int level, int suffix_index = 0, bool IS_LEAF_NODE = false) {
 
   Node* N = new Node;
   N->Lv = level;
@@ -35,6 +36,7 @@ Node* createTrieNode(int level, bool IS_LEAF_NODE = false) {
     cout << "new Node: level " << level << endl;
   } else {
     cout << "new Leaf: level " << level << endl;
+    N->suffix_index = suffix_index;
   }
   N->_color = 0;
   N->_next_edge = 0;
@@ -42,7 +44,7 @@ Node* createTrieNode(int level, bool IS_LEAF_NODE = false) {
 }
 
 
-void insertSuffix(Node* root, int* suffix_start) {
+void insertSuffix(Node* root, int* suffix_start, int suffix_index = 0) {
 
   Node* curr_node = root;
   int* curr_char = suffix_start;
@@ -52,7 +54,7 @@ void insertSuffix(Node* root, int* suffix_start) {
     lv++;
 
     if (!curr_node->edge_table[*curr_char]) {
-      curr_node->edge_table[*curr_char] = createTrieEdge(createTrieNode(lv, false), curr_char);
+      curr_node->edge_table[*curr_char] = createTrieEdge(createTrieNode(lv, 0 ,false), curr_char);
     }
 
     curr_node = curr_node->edge_table[*curr_char]->target_node;
@@ -61,12 +63,33 @@ void insertSuffix(Node* root, int* suffix_start) {
 
   if (*curr_char == INT_MAX) {
     if (!curr_node->edge_table[ALPHABET_SIZE - 1]) {
-      curr_node->edge_table[ALPHABET_SIZE - 1] = createTrieEdge(createTrieNode(lv + 1, true), curr_char);
+      curr_node->edge_table[ALPHABET_SIZE - 1] = createTrieEdge(createTrieNode(lv + 1, suffix_index ,true), curr_char);
     }
   }
 }
 
+void collectNodes(Node* root) {
+  queue<Node*> node_q;
+  node_q.push(root);
+  Node* curr = nullptr;
+  while (!node_q.empty()) {
+    curr = node_q.front();
+    node_q.pop();
+    if (curr->edge_table == nullptr) {
+      cout << "Leaf node encountered: " << curr->suffix_index << endl;
+      continue;
+    }
 
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+      if (curr->edge_table[i] != nullptr) {
+        curr->children.push_back(curr->edge_table[i]);
+        node_q.push(curr->edge_table[i]->target_node);
+      }
+    }
+    delete[] curr->edge_table;
+  }
+}
+/*
 void contract(Node* root) {
   stack<Edge*> edge_stack;
   edge_stack.push(createEdge(nullptr));
@@ -89,16 +112,23 @@ void contract(Node* root) {
     }
   }
 }
+*/
 
 int main() {
   int sample[] = {3,1,3,1,2,INT_MAX};
+  int sample2[] = {2, 1, 2, 3, 4, 3, INT_MAX};
+  // int sample_size = 6;
+  int sample_size = 7;
   //char sample[] = {'2', '1', '2', '3', '4', '3', '$', '\0'};
   int k = 0;
   Node* root = createTrieNode(k);
-  insertSuffix(root, sample);
-  insertSuffix(root, sample + 1);
-  insertSuffix(root, sample + 2);
-  insertSuffix(root, sample + 5);
+  for (int i = 0; i < sample_size; i++) {
+    insertSuffix(root, sample2 + i, i + 1);
+  }
+  collectNodes(root);
+  // insertSuffix(root, sample + 1);
+  // insertSuffix(root, sample + 2);
+  // insertSuffix(root, sample + 5);
   //contract(root);
   // for (int i = 0; i < root->children.size(); i++) {
   //   cout << root->children[i]->label_len << '\t' << root->children[i]->target->L << endl;
