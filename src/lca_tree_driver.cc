ccd096595f524ae87f4d70ab31dbbff3296a31d6
DoYouEvenStackSmash <manumoomaw@gmail.com>
2022-05-28 23:54:22 -0700

    added functions for updating even tree values

--- src/lca_tree_driver.cc
+++ src/lca_tree_driver.cc
@@ -3,6 +3,43 @@
 #include "compacted_trie.h"
 using namespace std;
 
+void even_sort(int* input_string, vector<int> A_ext, vector<int> &rv) {
+  map<int, vector<int>> m;
+  for(int i = 0; i < A_ext.size(); i++) {
+    if (A_ext[i] - 2 < 0)
+      continue;
+    m[input_string[A_ext[i] - 2]].push_back(A_ext[i]);
+  }
+
+  for (auto a : m) {
+    for (auto b : a.second)
+      rv.push_back(b - 1);
+  }
+}
+
+int RMQ(SparseTable* lca, int x, int y) {
+  int L = lca->rep[x];
+  int R = lca->rep[y];
+  if (R < L)
+    swap(R, L);
+  int j = lca->log_memo[R - L + 1];
+  return min(lca->st[L][j], lca->st[R - (1 << j) + 1][j]);
+}
+
+void evenLCP(int* str, SparseTable* lca, vector<int> &A_ext, vector<int> &LCP_ext) {
+  int L;
+  int R;
+  for (int i = 1; i < A_ext.size(); i++) {
+    L = A_ext[i - 1];
+    R = A_ext[i];
+    if (str[L - 1] != str[R - 1]) {
+      LCP_ext.push_back(0);
+    } else {
+      LCP_ext.push_back(RMQ(lca, L + 1, R + 1) + 1);
+    }
+  }
+}
+
 void LCA_trie() {
   S s;
 
@@ -34,6 +71,20 @@ void LCA_trie() {
   constructFullOddTrie(s.root, s.LCP_ext, s.A_ext, s.input_string.data());
 
   preprocessLCA(s);
+  S se;
+  even_sort(s.input_string.data(), s.A_ext, se.A_ext);
+  cout << "A_ext" << endl;
+  for (auto a : se.A_ext)
+    cout << a << " ";
+  cout << endl;
+
+  int* str = s.input_string.data();
+  SparseTable* lca = s.sp_tab;
+  evenLCP(str, s.sp_tab, se.A_ext, se.LCP_ext);
+  cout << "LCP_ext" << endl;
+  for (auto a : se.LCP_ext)
+    cout << a << " ";
+  cout << endl;
   deleteST(s.sp_tab);
   //displayTrie(s.root);
 
